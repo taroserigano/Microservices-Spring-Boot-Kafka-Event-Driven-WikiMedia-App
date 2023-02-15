99ecb2351defb0d71ac898f512269bda932a19c1
Taro Serigano <taroserigano@gmail.com>
2023-02-15 08:06:52 -0600

    Add files via upload

---
+++ kafka-consumer-database/src/main/java/net/java/springboot/repository/WikimediaDataRepository.java
@@ -0,0 +1,7 @@
+package net.java.springboot.repository;
+
+import net.java.springboot.entity.WikimediaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WikimediaDataRepository extends JpaRepository<WikimediaData, Long> {
+}
